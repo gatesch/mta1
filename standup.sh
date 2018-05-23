758472cfb2edd17be528ded03c64bbb57d4c9b27
alex <gatesch@gmail.com>
2018-05-23 10:45:57 +0800

    putting all together

---
+++ standup.sh
@@ -0,0 +1,5 @@
+docker build -t db ./db
+docker build -t tomcat ./tomcat
+docker network create atsea
+docker run -d --network atsea -p 8080:8080 --name tomcat tomcat
+docker run -d --network atsea --name db db
